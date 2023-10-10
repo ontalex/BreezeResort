828cf31aa1946811095fc43838ce55b291e213cf
Ontalex <ontalex@yandex.ru>
2023-10-10 10:44:51 +0300

    add: hotels routes, controllers

--- controllers/hotels.controller.js
+++ controllers/hotels.controller.js
@@ -1,5 +1,11 @@
 import db from "../db.js";
 
-class Hotels {}
+class Hotels {
+    addRoom(req, res) {}
+    getAllRooms(req, res) {}
+    deleteRoom(req, res) {}
+    getRoomInHotel(req, res) {}
+
+}
 
 export default new Hotels();
--- routes/hotels.routes.js
+++ routes/hotels.routes.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
+import hotelsController from "../controllers/hotels.controller";
 
 const hotels = Router();
+
+hotels.post("/hotel", hotelsController.addRoom);
+hotels.get("/hotels", hotelsController.getAllRooms);
+hotels.delete("/hotel/:id", hotelsController.deleteRoom);
+hotels.get("/hotel/:id/room/:idroom", hotelsController.getRoomInHotel);
 
 export default hotels;
