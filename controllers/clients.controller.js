9906c61dd0b764fd1fb977c1a47a0f624b4b50b6
Ontalex <ontalex@yandex.ru>
2023-10-10 10:44:41 +0300

    add: clients routes, controllers

--- controllers/clients.controller.js
+++ controllers/clients.controller.js
@@ -1,5 +1,27 @@
 import db from "../db.js";
 
-class Clients {}
+class Clients {
+
+    register(req, res) {
+
+    }
+
+    patchClient(req, res) {
+
+    }
+
+    deleteClient(req, res) {
+        
+    }
+
+    getRegisteredUser(req, res) {
+
+    }
+
+    getAllRoom(req, res) {
+        
+    }
+
+}
 
 export default new Clients();
--- routes/clients.routes.js
+++ routes/clients.routes.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import clientsController from "../controllers/clients.controller";
 
 const clients = Router();
+
+clients.post("/register", clientsController.register);
+clients.patch("/userdata/:id", clientsController.patchClient);
+clients.delete("/userdata/:id", clientsController.deleteClient);
+clients.get("/room/:id/userdata/:iduser", clientsController.getRegisteredUser);
+clients.get("/usersinroom", clientsController.getAllRoom);
 
 export default clients;
