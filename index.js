d3c6c6bd4f5b09e95aa31b48c462b4438a354859
Ontalex <ontalex@yandex.ru>
2023-10-10 09:40:28 +0300

    add: listener for auth

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import exp from 'express';
 import dotenv from 'dotenv';
 import test from './routes/test.routes.js';
+import auth from './routes/auth.routes.js';
 
 dotenv.config();
 
@@ -8,7 +9,7 @@ const app = exp();
 
 app.use(exp.json());
 
-app.use("/api", [test]);
+app.use("/api", [test, auth]);
 
 app.listen(process.env.SERVER_PORT || 8080, (err) => {
     if (err) {
