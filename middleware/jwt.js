6c0db0f6746b30100ffc4d6f9572ed9967db3312
Ontalex <ontalex@yandex.ru>
2023-10-11 10:13:02 +0300

    update: вшивание userID досле проверки токена

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -19,6 +19,8 @@ class JWTVer {
     check(req, res, next) {
         try {
 
+            console.log('>>> CHECKING TOKEN PROCESS....');
+
             let token = req.headers.authorization.split(' ')[1];
 
             jwt.verify(token, process.env.SECRETKEY, (err, decod) => {
@@ -28,13 +30,15 @@ class JWTVer {
                     console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
                     res.status(403).json(unauth);
                 } else {
-                    next(req, res);
+                    console.log('>>> CHECKING TOKEN PROCESS - ACCESS');
+                    console.log('>>> DECODE', decod);
+                    req.decode_body = decod.data;
+                    next();
                 }
 
             });
 
         } catch (error) {
-
             res.status(403).json(unauth);
 
         }
