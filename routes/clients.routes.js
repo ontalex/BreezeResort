831f5462bd77edbcdd2de2bbd30e9e596260f680
Aleksey Borisov <loja.bobaev2000@yandex.ru>
2023-10-14 20:43:49 +0300

    update: type endpoint request for clients

--- routes/clients.routes.js
+++ routes/clients.routes.js
@@ -8,7 +8,7 @@ const clients = Router();
 clients.post("/register", jwt.check, clientsController.register);
 clients.patch("/userdata/:id", jwt.check, clientsController.patchClient);
 clients.delete("/userdata/:id", jwt.check, clientsController.deleteClient);
-clients.get("/room/:id/userdata/:iduser", jwt.check, clientsController.getRegisteredUser);
-clients.get("/usersinroom", jwt.check, clientsController.getAllRoom);
+clients.get("/room/:id/userdata/:iduser", jwt.check, clientsController.patchRegisteredUser);
+clients.post("/usersinroom", jwt.check, clientsController.getAllInRoom);
 
 export default clients;
