44bd8a3e6e3be4ca7ef8edc14c2223d67486cd89
Aleksey Borisov <loja.bobaev2000@yandex.ru>
2023-10-16 12:46:25 +0300

    update: db

--- TZ/BreezeResort.sql
+++ TZ/BreezeResort.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Окт 12 2023 г., 11:06
+-- Время создания: Окт 16 2023 г., 12:44
 -- Версия сервера: 8.0.30
--- Версия PHP: 8.1.9
+-- Версия PHP: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База данных: `BreezeResort`
+-- База данных: `breezeresort`
 --
 
 -- --------------------------------------------------------
@@ -29,12 +29,12 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `clients` (
   `id` int NOT NULL,
-  `fio` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `phone` varchar(20) NOT NULL,
-  `id_childata` int NOT NULL,
-  `birth_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `fio` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `birth_date` date DEFAULT NULL,
+  `id_childata` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
@@ -44,17 +44,19 @@ CREATE TABLE `clients` (
 
 CREATE TABLE `hotels` (
   `id` int NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `number` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `name` varchar(255) DEFAULT NULL,
+  `number` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Дамп данных таблицы `hotels`
 --
 
 INSERT INTO `hotels` (`id`, `name`, `number`) VALUES
-(1, 'Главный-ЮГ', 321),
-(2, 'Главный-Север', 322);
+(1, 'Главный-Север', 100),
+(2, 'Главный-Юг', 200),
+(3, 'Омск-1', 300),
+(4, 'Омск-2', 301);
 
 -- --------------------------------------------------------
 
@@ -64,10 +66,22 @@ INSERT INTO `hotels` (`id`, `name`, `number`) VALUES
 
 CREATE TABLE `rooms` (
   `id` int NOT NULL,
-  `idhotel` int NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `desc_data` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `name` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `desc_data` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `idhotel` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Дамп данных таблицы `rooms`
+--
+
+INSERT INTO `rooms` (`id`, `name`, `desc_data`, `idhotel`) VALUES
+(1, 'room 1', 'some room', 1),
+(2, 'room 2', 'some room', 1),
+(3, 'room 3', 'some room', NULL),
+(4, 'room 4', 'some room', NULL),
+(5, 'room 5', 'some room', NULL),
+(6, 'room 6', 'some room', 2);
 
 -- --------------------------------------------------------
 
@@ -77,18 +91,16 @@ CREATE TABLE `rooms` (
 
 CREATE TABLE `users` (
   `id` int NOT NULL,
-  `username` varchar(120) NOT NULL,
-  `password` varchar(120) NOT NULL,
-  `idhotel` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `username` varchar(120) DEFAULT NULL,
+  `password` varchar(120) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Дамп данных таблицы `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `idhotel`) VALUES
-(1, 'Ontalex', '123qwe', 1),
-(2, 'Lex', '123qwe', 2);
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'Ontalex', '123qwe');
 
 --
 -- Индексы сохранённых таблиц
@@ -99,27 +111,32 @@ INSERT INTO `users` (`id`, `username`, `password`, `idhotel`) VALUES
 --
 ALTER TABLE `clients`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_childata` (`id_childata`);
+  ADD UNIQUE KEY `email_UNIQUE` (`email`),
+  ADD UNIQUE KEY `phone_UNIQUE` (`phone`),
+  ADD KEY `fk_clients_rooms1_idx` (`id_childata`);
 
 --
 -- Индексы таблицы `hotels`
 --
 ALTER TABLE `hotels`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_UNIQUE` (`id`),
+  ADD UNIQUE KEY `name_UNIQUE` (`name`),
+  ADD UNIQUE KEY `number_UNIQUE` (`number`);
 
 --
 -- Индексы таблицы `rooms`
 --
 ALTER TABLE `rooms`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `idhotel` (`idhotel`);
+  ADD KEY `fk_rooms_hotels1_idx` (`idhotel`);
 
 --
 -- Индексы таблицы `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `idhotel` (`idhotel`);
+  ADD UNIQUE KEY `id_UNIQUE` (`id`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -135,19 +152,19 @@ ALTER TABLE `clients`
 -- AUTO_INCREMENT для таблицы `hotels`
 --
 ALTER TABLE `hotels`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `rooms`
 --
 ALTER TABLE `rooms`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -157,19 +174,13 @@ ALTER TABLE `users`
 -- Ограничения внешнего ключа таблицы `clients`
 --
 ALTER TABLE `clients`
-  ADD CONSTRAINT `clients_ibfk_1` FOREIGN KEY (`id_childata`) REFERENCES `rooms` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `fk_clients_rooms1` FOREIGN KEY (`id_childata`) REFERENCES `rooms` (`id`) ON DELETE CASCADE;
 
 --
 -- Ограничения внешнего ключа таблицы `rooms`
 --
 ALTER TABLE `rooms`
-  ADD CONSTRAINT `rooms_ibfk_1` FOREIGN KEY (`idhotel`) REFERENCES `hotels` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Ограничения внешнего ключа таблицы `users`
---
-ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`idhotel`) REFERENCES `hotels` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `fk_rooms_hotels1` FOREIGN KEY (`idhotel`) REFERENCES `hotels` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
