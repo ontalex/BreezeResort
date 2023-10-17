7b9ead5f5b353bf4ca04c57663786f1bbf28c750
Ontalex <ontalex@yandex.ru>
2023-10-17 10:51:27 +0300

    add: cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import exp from 'express';
+import cors from "cors";
 import dotenv from 'dotenv';
-import test from './routes/test.routes.js';
 import auth from './routes/auth.routes.js';
 import rooms from './routes/rooms.routes.js';
 import clients from './routes/clients.routes.js';
@@ -8,11 +8,15 @@ import hotels from './routes/hotels.routes.js';
 
 dotenv.config();
 
+let corsConfig = {
+    origin: "*"
+};
+
 const app = exp();
 
 app.use(exp.json());
 
-app.use("/api", [test, auth, rooms, clients, hotels]);
+app.use("/api", cors(corsConfig), [auth, rooms, clients, hotels]);
 
 app.listen(process.env.SERVER_PORT || 8080, (err) => {
     if (err) {
